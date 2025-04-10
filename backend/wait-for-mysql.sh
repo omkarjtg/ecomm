652743c3a70bcc89d5811f2222034dc64d48b5c7
omkarjtg <112477787+omkarjtg@users.noreply.github.com>
2025-04-10 21:38:38 +0530

    Update wait-for-mysql.sh

--- backend/wait-for-mysql.sh
+++ backend/wait-for-mysql.sh
@@ -2,14 +2,13 @@
 
 set -e
 
-host="$1"
-shift
-cmd="$@"
+host="$MYSQL_HOST"
+port="$MYSQL_PORT"
 
-until mysql -h "$host" -u root -proot -e "SELECT 1" > /dev/null 2>&1; do
-  >&2 echo "MySQL is unavailable - sleeping"
+until nc -z -v -w30 "$host" "$port"; do
+  echo "MySQL is unavailable - sleeping"
   sleep 2
 done
 
->&2 echo "MySQL is up - executing command"
-exec $cmd
+echo "MySQL is up - executing command"
+exec "$@"
